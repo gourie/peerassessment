0f6283039cfb057e36100cc093063292a403d796
Joeri Nicolaes <joeri.nicolaes@ucb.com>
2020-06-21 21:54:21 +0200

    - simplest way to get scores data_matrix (directly read as pandas dataframe) - TODO: remove or complete other methods for building scores graph (not sure this is still needed)

--- get_scores.py
+++ get_scores.py
@@ -4,8 +4,32 @@ Script for getting scores data (mostly problem specific but I generalized a few 
 :copyright: (c) 2020, Joeri Nicolaes
 :license: BSD license, see LICENSE for more details.
 """
-import ArgumentParser
+import argparse
+import numpy as np
+import pandas as pd
 from peerscore import PeerScoresGraph, Peer
+
+
+def read_df(fn):
+    """
+    Reads xlsx scores into OrderedDict pandas dataframe
+    Contains specific pre-processing for scores file from KUL-H02A5a
+
+    :param fn: str
+    :return: OrderedDict object returned by pandas (one df for each sheet)
+    """
+    def change_name(name_str):
+        return name_str.replace('Ontvangen door ', '')
+    df = pd.read_excel(fn, sheet_name=None)
+    for k in df.keys():
+        # NA is represented as 6 > change to np.nan
+        df[k].replace(6, np.nan)
+        df[k].Naam = df[k].Naam.apply(change_name)
+        cols = [c for c in df[k].columns.values if 'B' in c]
+        [df[k].rename(columns={c: df[k].Naam[df[k].Id == c].values[0]}, inplace=True) for c in cols]
+        df[k] = df[k].drop(columns=['Id'])
+    full_df = pd.concat([df[k] for k in df.keys()], sort=False)
+    return full_df.set_index('Naam')    #.sort_index(axis=1)
 
 
 def get_peers_from_database(fn):
@@ -20,10 +44,15 @@ def get_peers_from_database(fn):
     :param fn: str
     :return: list of Peer objects
     """
-    return [Peer()]
+    df = read_df(fn)
+    return [Peer(peer_unique_id=i, peer_friendly_name=n) for i,n in enumerate(df.index.values)], df
 
 
-def get_scores_from_peer_in_database(peer_id, fn):
+def tuple_series_index_and_values(s):
+    return list(zip(s, s.index))
+
+
+def get_scores_from_peer_in_database(peer_friendly_name, df):
     """
     Get scores given by peer_id in database
     This function is specific to your scores database file and should be overwritten !!!!
@@ -32,12 +61,13 @@ def get_scores_from_peer_in_database(peer_id, fn):
         def get_scores_from_peer_in_database(peer_id, fn):
             return [(score, Peer())]
 
-    :param peer_id: str
-    :param fn: str, scores database file
+    :param peer_friendly_name: str
+    :param df: pandas.DataFrame
     :return: list, each entry is a tuple (score, to_peer) with
         score (int or float are supported)
     """
-    return [(3, Peer())]
+    scores_given_by_peer = df[peer_friendly_name][~df[peer_friendly_name].isnull()]
+    return tuple_series_index_and_values(scores_given_by_peer)
 
 
 def get_peerscores_from_database(fn):
@@ -50,19 +80,21 @@ def get_peerscores_from_database(fn):
     """
     # 1. initialize graph with Peer objects as nodes
     inputscores_graph = PeerScoresGraph()
-    for peer_obj in get_peers_from_database(fn):
+    peers, df = get_peers_from_database(fn)
+    for peer_obj in peers:
         inputscores_graph.add_peer(peer_obj)
     # 2. update graph with scores as edges
-    for peer_obj in inputscores_graph.nodes():
-        scores_list = get_scores_from_peer_in_database(peer_obj.peer_unique_id, fn)
+    for from_peer in inputscores_graph.nodes():
+        scores_list = get_scores_from_peer_in_database(from_peer.peer_friendly_name, df)
         for score, to_peer in scores_list:
-            inputscores_graph.add_score(score, peer_obj, to_peer)
+            inputscores_graph.add_score(score, from_peer, Peer(peer_unique_id=-1, peer_friendly_name=to_peer))
     return inputscores_graph
 
 
 def main(scores_fn):
-    peer_inputscores_graph = get_peerscores_from_database(scores_fn)
-    return
+    # peer_inputscores_graph = get_peerscores_from_database(scores_fn)
+    data_matrix = read_df(scores_fn)
+    print(data_matrix.shape)
 
 
 if __name__ == '__main__':
